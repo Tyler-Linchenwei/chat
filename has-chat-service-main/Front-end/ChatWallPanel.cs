ee40588bfe2bc0de8f9cb4d4b7c46e9b68a9fae8
Tyler-Linchenwei <114295738+Tyler-Linchenwei@users.noreply.github.com>
2023-11-26 20:10:46 +0800

    Add files via upload

---
+++ has-chat-service-main/Front-end/ChatWallPanel.cs
@@ -0,0 +1,39 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Erinn
+{
+    public sealed class ChatWallPanel : MonoBehaviour
+    {
+        public TMP_Text ChatOutput;
+        public TMP_InputField InputField;
+        public Button SendButton;
+
+        private void Awake()
+        {
+            Register();
+        }
+
+        public void Register()
+        {
+            SendButton.onClick.AddListener(OnSendButton);
+            NetworkTransport.Singleton.RegisterHandler<ChatWallResponse>(OnChatWallResponse);
+            NetworkTransport.Singleton.RegisterHandler<ChatWallMessage>(OnChatWallMessage);
+            return;
+
+            void OnSendButton()
+            {
+                var content = InputField.text;
+                if (string.IsNullOrEmpty(content))
+                    return;
+                NetworkTransport.Singleton.Send(new ChatWallRequest(content));
+                InputField.text = "";
+            }
+
+            void OnChatWallResponse(ChatWallResponse response) => ChatOutput.text += $"\r\n{response.Error}";
+
+            void OnChatWallMessage(ChatWallMessage message) => ChatOutput.text += $"\r\n{message.Username}: {message.Content}";
+        }
+    }
+}
---
+++ has-chat-service-main/Front-end/LoginPanel.cs
@@ -0,0 +1,128 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Erinn
+{
+    public sealed class LoginPanel : MonoBehaviour
+    {
+        public TMP_InputField LoginEmail;
+        public TMP_InputField LoginUserpassword;
+        public Button LoginButton;
+        public TMP_InputField RegisterEmail;
+        public TMP_InputField RegisterUsername;
+        public TMP_InputField RegisterUserpassword;
+        public TMP_InputField RegisterCode;
+        public Button RegisterSendCodeButton;
+        public Button RegisterButton;
+        public GameObject Tip;
+        public TMP_Text TipText;
+
+        public GameObject ChatWall;
+        
+        private void Start() => Register();
+
+        public void Register()
+        {
+            NetworkTransport.Singleton.RegisterHandler<LoginResponse>(OnLoginResponse);
+            NetworkTransport.Singleton.RegisterHandler<RegisterEmailcodeResponse>(OnEmailcodeResponse);
+            NetworkTransport.Singleton.RegisterHandler<RegisterResponse>(OnRegisterResponse);
+            LoginButton.onClick.AddListener(OnLoginButton);
+            RegisterSendCodeButton.onClick.AddListener(OnRegisterSendCodeButton);
+            RegisterButton.onClick.AddListener(OnRegisterButton);
+            return;
+
+            void OnLoginButton()
+            {
+                var email = LoginEmail.text;
+                if (string.IsNullOrEmpty(email))
+                {
+                    OnTip("邮箱不能为空");
+                    return;
+                }
+
+                var userpassword = LoginUserpassword.text;
+                if (string.IsNullOrEmpty(userpassword))
+                {
+                    OnTip("密码不能为空");
+                    return;
+                }
+
+                var request = new LoginRequest(email, userpassword);
+                NetworkTransport.Singleton.Send(request);
+            }
+
+            void OnRegisterSendCodeButton()
+            {
+                var email = RegisterEmail.text;
+                if (string.IsNullOrEmpty(email))
+                {
+                    OnTip("邮箱不能为空");
+                    return;
+                }
+
+                NetworkTransport.Singleton.Send(new RegisterEmailcodeRequest(email));
+            }
+
+            void OnRegisterButton()
+            {
+                var email = RegisterEmail.text;
+                if (string.IsNullOrEmpty(email))
+                {
+                    OnTip("邮箱不能为空");
+                    return;
+                }
+
+                var username = RegisterUsername.text;
+                if (string.IsNullOrEmpty(username))
+                {
+                    OnTip("用户名不能为空");
+                    return;
+                }
+
+                var userpassword = RegisterUserpassword.text;
+                if (string.IsNullOrEmpty(userpassword))
+                {
+                    OnTip("密码不能为空");
+                    return;
+                }
+
+                if (userpassword.Length is < 8 or > 15)
+                {
+                    OnTip("密码长度应为8到15位");
+                    return;
+                }
+
+                var emailcode = RegisterCode.text;
+                if (string.IsNullOrEmpty(emailcode))
+                {
+                    OnTip("验证码不能为空");
+                    return;
+                }
+
+                NetworkTransport.Singleton.Send(new RegisterRequest(email, username, userpassword, emailcode));
+            }
+
+            void OnLoginResponse(LoginResponse response)
+            {
+                if (response.Success)
+                {
+                    ChatWall.SetActive(true);
+                    gameObject.SetActive(false);
+                    return;
+                }
+                OnTip(response.Error);
+            }
+
+            void OnEmailcodeResponse(RegisterEmailcodeResponse response) => OnTip(response.Error);
+
+            void OnRegisterResponse(RegisterResponse response) => OnTip(response.Error);
+
+            void OnTip(string message)
+            {
+                TipText.text = message;
+                Tip.SetActive(true);
+            }
+        }
+    }
+}
